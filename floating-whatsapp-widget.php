ea28232e7a42f09919e37a99478d64ec3b89b1b2
Mthokozisi Dhlamini <63953399+sthabiso-iv@users.noreply.github.com>
2024-03-16 22:38:51 +0200

    Update floating-whatsapp-widget.php

--- floating-whatsapp-widget.php
+++ floating-whatsapp-widget.php
@@ -2,25 +2,27 @@
 /*
 Plugin Name: Floating WhatsApp Widget
 Description: Creates a floating WhatsApp widget on the front-end.
-Version: 1.1
+Version: 1.2
 Author: DesignThat Cloud (Mthokozisi Dhlamini)
+Author URI: https://designthat.cloud
+Plugin URI: https://github.com/DesignThat/FloatingWhatsAppWidget
 */
 
-// Add the plugin settings page to the admin menu
+
 function floating_whatsapp_widget_add_settings_page() {
     add_options_page( 'WhatsApp Widget Settings', 'WhatsApp Widget', 'manage_options', 'floating-whatsapp-widget', 'floating_whatsapp_widget_render_settings_page' );
 }
 add_action( 'admin_menu', 'floating_whatsapp_widget_add_settings_page' );
 
-// Render the plugin settings page
+
 function floating_whatsapp_widget_render_settings_page() {
-    $whatsapp_number = get_option( 'floating_whatsapp_widget_number' ); // Get the WhatsApp number from plugin settings
-    $position = get_option( 'floating_whatsapp_widget_position', 'right' ); // Get the widget position from plugin settings
-    $placement = get_option( 'floating_whatsapp_widget_placement', 'bottom' ); // Get the widget placement from plugin settings
-    $widget_color = get_option( 'floating_whatsapp_widget_color', '#25D366' ); // Get the widget color from plugin settings
-    $widget_radius = get_option( 'floating_whatsapp_widget_radius', 50 ); // Get the widget border radius from plugin settings
-    $widget_shadow = get_option( 'floating_whatsapp_widget_shadow', 5 ); // Get the widget box shadow from plugin settings
-    $widget_font_size = get_option( 'floating_whatsapp_widget_font_size', 24 ); // Get the widget font size from plugin settings
+    $whatsapp_number = get_option( 'floating_whatsapp_widget_number' ); 
+    $position = get_option( 'floating_whatsapp_widget_position', 'right' ); 
+    $placement = get_option( 'floating_whatsapp_widget_placement', 'bottom' ); 
+    $widget_color = get_option( 'floating_whatsapp_widget_color', '#25D366' );
+    $widget_radius = get_option( 'floating_whatsapp_widget_radius', 50 ); 
+    $widget_shadow = get_option( 'floating_whatsapp_widget_shadow', 5 ); 
+    $widget_font_size = get_option( 'floating_whatsapp_widget_font_size', 24 );
     ?>
     <div class="wrap">
         <h1>WhatsApp Widget Settings</h1>
@@ -29,19 +31,12 @@ function floating_whatsapp_widget_render_settings_page() {
             settings_fields( 'floating_whatsapp_widget_settings' );
             do_settings_sections( 'floating-whatsapp-widget' );
             ?>
-            <h2>Widget Preview</h2>
-            <div id="widget-preview" class="floating-whatsapp-widget" style="background-color: <?php echo esc_attr( $widget_color ); ?>; border-radius: <?php echo esc_attr( $widget_radius ); ?>px; box-shadow: 0 <?php echo esc_attr( $widget_shadow ); ?>px <?php echo esc_attr( $widget_shadow ); ?>px rgba(0, 0, 0, 0.3); font-size: <?php echo esc_attr( $widget_font_size ); ?>px;">
-                <a href="https://wa.me/<?php echo esc_attr( $whatsapp_number ); ?>" target="_blank">
-                    <i class="fa fa-whatsapp"></i>
-                </a>
-            </div>
-            <h2>Widget Settings</h2>
+
+            <h2>Widget Appearance</h2>
             <table class="form-table">
                 <tr valign="top">
                     <th scope="row">WhatsApp Number</th>
-                    <td>
-                        <input type="text" name="floating_whatsapp_widget_number" value="<?php echo esc_attr( $whatsapp_number ); ?>" />
-                    </td>
+                    <td><input type="text" name="floating_whatsapp_widget_number" value="<?php echo esc_attr( $whatsapp_number ); ?>" /></td>
                 </tr>
                 <tr valign="top">
                     <th scope="row">Position</th>
@@ -63,48 +58,38 @@ function floating_whatsapp_widget_render_settings_page() {
                 </tr>
                 <tr valign="top">
                     <th scope="row">Widget Color</th>
-                    <td>
-                        <input type="text" name="floating_whatsapp_widget_color" class="floating-whatsapp-widget-color-picker" value="<?php echo esc_attr( $widget_color ); ?>" />
-                    </td>
+                    <td><input type="text" name="floating_whatsapp_widget_color" class="floating-whatsapp-widget-color-picker" value="<?php echo esc_attr( $widget_color ); ?>" /></td>
                 </tr>
                 <tr valign="top">
                     <th scope="row">Widget Border Radius</th>
-                    <td>
-                        <input type="number" name="floating_whatsapp_widget_radius" value="<?php echo esc_attr( $widget_radius ); ?>" />
-                    </td>
+                    <td><input type="number" name="floating_whatsapp_widget_radius" value="<?php echo esc_attr( $widget_radius ); ?>" /></td>
                 </tr>
                 <tr valign="top">
                     <th scope="row">Widget Box Shadow</th>
-                    <td>
-                        <input type="number" name="floating_whatsapp_widget_shadow" value="<?php echo esc_attr( $widget_shadow ); ?>" />
-                    </td>
+                    <td><input type="number" name="floating_whatsapp_widget_shadow" value="<?php echo esc_attr( $widget_shadow ); ?>" /></td>
                 </tr>
                 <tr valign="top">
                     <th scope="row">Widget Font Size</th>
-                    <td>
-                        <input type="number" name="floating_whatsapp_widget_font_size" value="<?php echo esc_attr( $widget_font_size ); ?>" />
-                    </td>
+                    <td><input type="number" name="floating_whatsapp_widget_font_size" value="<?php echo esc_attr( $widget_font_size ); ?>" /></td>
                 </tr>
             </table>
+           
+            <h2>Widget Preview</h2>
+            <div id="widget-preview" class="floating-whatsapp-widget" style="background-color: <?php echo esc_attr( $widget_color ); ?>; border-radius: <?php echo esc_attr( $widget_radius ); ?>px; box-shadow: 0 <?php echo esc_attr( $widget_shadow ); ?>px <?php echo esc_attr( $widget_shadow ); ?>px rgba(0, 0, 0, 0.3); font-size: <?php echo esc_attr( $widget_font_size ); ?>px;">     <a href="https://wa.me/<?php echo esc_attr( $whatsapp_number ); ?>" target="_blank"><i class="fa fa-whatsapp"></i></a></div>
             <?php submit_button(); ?>
         </form>
     </div>
     <?php
 }
 
-// Register plugin settings
+
 function floating_whatsapp_widget_register_settings() {
     register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_number' );
-    register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_position' );
-    register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_placement' );
-    register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_color' );
-    register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_radius' );
-    register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_shadow' );
-    register_setting( 'floating_whatsapp_widget_settings', 'floating_whatsapp_widget_font_size' );
+    // other settings
 }
 add_action( 'admin_init', 'floating_whatsapp_widget_register_settings' );
 
-// Enqueue the necessary scripts and styles
+
 function floating_whatsapp_widget_scripts() {
     wp_enqueue_style( 'wp-color-picker' );
     wp_enqueue_script( 'wp-color-picker' );
@@ -115,19 +100,6 @@ add_action( 'admin_enqueue_scripts', 'floating_whatsapp_widget_scripts' );
 
 // Add the WhatsApp widget HTML to the footer
 function floating_whatsapp_widget_html() {
-    $whatsapp_number = get_option( 'floating_whatsapp_widget_number' ); // Get the WhatsApp number from plugin settings
-    $position = get_option( 'floating_whatsapp_widget_position', 'right' ); // Get the widget position from plugin settings
-    $placement = get_option( 'floating_whatsapp_widget_placement', 'bottom' ); // Get the widget placement from plugin settings
-    $widget_color = get_option( 'floating_whatsapp_widget_color', '#25D366' ); // Get the widget color from plugin settings
-    $widget_radius = get_option( 'floating_whatsapp_widget_radius', 50 ); // Get the widget border radius from plugin settings
-    $widget_shadow = get_option( 'floating_whatsapp_widget_shadow', 5 ); // Get the widget box shadow from plugin settings
-    $widget_font_size = get_option( 'floating_whatsapp_widget_font_size', 24 ); // Get the widget font size from plugin settings
-    ?>
-    <div id="floating-whatsapp-widget" class="floating-whatsapp-widget" style="background-color: <?php echo esc_attr( $widget_color ); ?>; border-radius: <?php echo esc_attr( $widget_radius ); ?>px; box-shadow: 0 <?php echo esc_attr( $widget_shadow ); ?>px <?php echo esc_attr( $widget_shadow ); ?>px rgba(0, 0, 0, 0.3); font-size: <?php echo esc_attr( $widget_font_size ); ?>px; <?php echo esc_attr( $position . ': ' . $placement . ';' ); ?>">
-        <a href="https://wa.me/<?php echo esc_attr( $whatsapp_number ); ?>" target="_blank">
-            <i class="fa fa-whatsapp"></i>
-        </a>
-    </div>
-    <?php
+    
 }
 add_action( 'wp_footer', 'floating_whatsapp_widget_html' );
